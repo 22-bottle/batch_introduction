9591b62975a1b99fa21976c53e9f1cdb21188f9c
이희병 <gmlquddl67@naver.com>
2024-01-25 08:43:51 +0900

    0125

---
+++ SpringBatchTutorial/src/main/java/com/example/SpringBatchTutorial/job/HelloWorldJobConfig.java
@@ -0,0 +1,20 @@
+package com.example.SpringBatchTutorial.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+
+    @Autowired
+    private Jobbuilderfactor
+
+
+
+
+
+
+}
